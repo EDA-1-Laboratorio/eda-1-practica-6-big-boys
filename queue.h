77e2f31094fcc1b2c8ae2a308dbd7f37f4ec082e
mailcasanas-max <mailcasanas@gmail.com>
2026-03-20 08:22:16 -0600

    Update queue.h

--- queue.h
+++ queue.h
@@ -1,36 +1,40 @@
-#include <assert.h> 
-#include <stdio.h> 
-#include <stdlib.h> 
+/* double_linked_queue.h */
+#ifndef DOUBLE_LINKED_QUEUE_H
+#define DOUBLE_LINKED_QUEUE_H
 
-#define    EMPTY    0 
-#define    FULL     10000 
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef    unsigned int     data; 
-typedef    enum {FALSO, VERDADERO}   boolean; 
+#define    EMPTY    0
+#define    FULL     10000
 
-struct dl_elem {           
-    data            d; 
-    struct dl_elem  *next;  // Apunta al siguiente elemento 
-    struct dl_elem  *prev;  // Apunta al elemento anterior (¡Nuevo!) 
-}; 
+typedef    unsigned int     data;
+typedef    enum {FALSO, VERDADERO}   boolean;
 
-typedef    struct dl_elem    dl_elem; 
+struct dl_elem {          
+    data            d;
+    struct dl_elem  *next;  // Apunta al siguiente elemento
+    struct dl_elem  *prev;  // Apunta al elemento anterior (¡Nuevo!)
+};
 
-struct dl_queue { 
-    int         cnt;                             
-    dl_elem     *head;                           
-    dl_elem     *tail;                           
-}; 
+typedef    struct dl_elem    dl_elem;
 
-typedef     struct dl_queue  dl_queue;         
+struct dl_queue {
+    int         cnt;                            
+    dl_elem     *head;                          
+    dl_elem     *tail;                          
+};
 
-/* Operaciones básicas de colas doblemente ligadas */ 
-void        dl_initialize(dl_queue *q);                
-void        dl_enqueue(data d, dl_queue *q);           
-data        dl_dequeue(dl_queue *q);                   
-data        dl_head(const dl_queue *q);                
-data        dl_tail(const dl_queue *q);                
-boolean     dl_empty(const dl_queue *q);               
-boolean     dl_full(const dl_queue *q);                
+typedef     struct dl_queue  dl_queue;        
+
+/* Operaciones básicas de colas doblemente ligadas */
+void        dl_initialize(dl_queue *q);               
+void        dl_enqueue(data d, dl_queue *q);          
+data        dl_dequeue(dl_queue *q);                  
+data        dl_head(const dl_queue *q);               
+data        dl_tail(const dl_queue *q);               
+boolean     dl_empty(const dl_queue *q);              
+boolean     dl_full(const dl_queue *q);               
 
 #endif
